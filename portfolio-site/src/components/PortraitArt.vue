b4a3eddebd569010977c8714a398ffeb40d14e9a
ATaylorN <amandanagies@yahoo.com>
2023-10-11 19:11:25 -0400

    finally getting these images uniform

--- portfolio-site/src/components/PortraitArt.vue
+++ portfolio-site/src/components/PortraitArt.vue
@@ -2,70 +2,70 @@
 <div class="portraits-page">
   <div class="portraits">
     
-        <div class="card" style="width:250px">
+        <div class="card">
          <img class="card-img-top" src="../assets/Images/portraits/nux.jpg" alt="Nux">
          <div class="card-img-overlay">
         <h4 class="card-title">Nux</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/nux2.jpg" alt="Nux">
         <div class="card-img-overlay">
         <h4 class="card-title">Nux</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/chester_bennington.jpg" alt="Chester Bennington">
         <div class="card-img-overlay">
         <h4 class="card-title">Linkin Park - Chester Bennington</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/eleventh_doctor.jpg" alt="Dr.Who - 11th Doctor">
         <div class="card-img-overlay">
         <h4 class="card-title">Dr.Who - 11th Doctor</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/jessica_jones.jpg" alt="Jessica Jones & Kilgrave">
         <div class="card-img-overlay">
         <h4 class="card-title">Jessica Jones & Kilgrave</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/cutiepiesensei_cosplay.jpg" alt="Cutiepiesensei">
         <div class="card-img-overlay">
         <h4 class="card-title">Black Panther Cosplay - Cutiepiesensei</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/loki_of_asgard.jpg" alt="Loki">
         <div class="card-img-overlay">
         <h4 class="card-title">Marvel - Loki</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/newly_weds.jpg" alt="Newly Weds Commission">
         <div class="card-img-overlay">
         <h4 class="card-title">Commission - Newly Weds</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/tifa_lockhart.jpg" alt="FFVII - Tifa">
         <div class="card-img-overlay">
         <h4 class="card-title">FFVII - Tifa</h4>
         </div>
         </div>
 
-        <div class="card" style="width:250px">
+        <div class="card">
          <img class="card-img-top" src="../assets/Images/portraits/aerith_gainsborough.jpg" alt="FFVII - Aerith">
          <div class="card-img-overlay">
         <h4 class="card-title">FFVII - Aerith</h4>
@@ -85,7 +85,15 @@ export default {
 </script>
 
 <style scoped>
-
+.card img{
+    width: 250px;
+    height: 310px;
+    object-fit: cover;
+}
+.card{
+    box-shadow: 0px 5px 20px 0px black;
+    border: none;
+}
 .portraits{
     padding: 4rem;
     display: flex;
