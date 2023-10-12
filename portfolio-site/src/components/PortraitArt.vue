5429aabb424e1f0b51d5b82025276f559aabf4ef
ATaylorN <amandanagies@yahoo.com>
2023-10-11 23:32:32 -0400

    more descriptions

--- portfolio-site/src/components/PortraitArt.vue
+++ portfolio-site/src/components/PortraitArt.vue
@@ -15,7 +15,7 @@
         <div class="modal-content">
         <div class="modal-header">
             <h4 class="modal-title">Mad Max: Fury Road - Nux</h4>
-            <p>"What a day! What a lovely day! -Nux"</p>
+            <p>"If I'm gonna die, I'm gonna die historic on the Fury Road!" -Nux</p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
@@ -45,6 +45,8 @@
         <div class="modal-content">
         <div class="modal-header">
             <h4 class="modal-title">Mad Max: Fury Road - Nux</h4>
+            <p>"Oh, what a day...what a lovely day! -Nux"</p>
+            <p></p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
@@ -74,7 +76,8 @@
         <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
-            <h4 class="modal-title">Linkin Park - Chester Bennington</h4>
+            <h4 class="modal-title">Linkin Park - Chester Bennington (1976-2017)</h4>
+            <p>"Life leaves us blind, love keeps us kind" -Chester Bennington</p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
@@ -104,14 +107,18 @@
         <div class="modal-content">
         <div class="modal-header">
             <h4 class="modal-title">Doctor Who - 11th Doctor</h4>
+            <p>"Bowties are cool!" -Matt Smith as the Doctor</p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
             <div v-show="showImage"><img class="card-img-top" src="../assets/Images/portraits/eleventh_doctor.jpg" alt="Dr.Who - 11th Doctor"></div>
-            <div v-show="showDescription"><p v-show="showDescription">Insert description here...</p></div>
+            <div v-show="showDescription"><p v-show="showDescription">Doctor Who is a British sci-fi TV series and has also been named TV's longest-running sci-fi show in the
+                world according to the Guinness World Records. It's essentially about an alien referred to as a 'Time Lord' named simply "the Doctor" who travels through space
+                and time, saving the universe. If the Doctor is severely injured, they have the ability to regenerate which is why there's been several actors who have played the
+                role of the Doctor over the years. The 11th Doctor was played by Matt Smith and is one of my favorites.</p></div>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-danger" @click="toggleDescription()">Description</button>
+        <button type="button" class="btn btn-danger" @click="toggleDescription()">Toggle Description</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="resetImg()">Close</button>
         </div>
         </div>
@@ -130,15 +137,19 @@
         <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
-            <h4 class="modal-title">Marvel's Jessica Jones - Jessica Jones & Kilgrave</h4>
+            <h4 class="modal-title">Marvel's Jessica Jones & Kilgrave</h4>
+            <p>"Would you put day drinking under experience or special abilities?" -Krysten Ritter as Jessica Jones</p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
             <div v-show="showImage"><img class="card-img-top" src="../assets/Images/portraits/jessica_jones.jpg" alt="Jessica Jones & Kilgrave"></div>
-            <div v-show="showDescription"><p v-show="showDescription">Insert description here...</p></div>
+            <div v-show="showDescription"><p v-show="showDescription">Jessica Jones is a character in the Marvel Universe and part of the Defenders.
+                She's an ex-superhero turned private investigator who suffers from PTSD played by the actress Krysten Ritter. Kilgrave, played by David Tennant,
+                has the ability to control minds and is the reason why Jessica has PTSD and gave up on being a hero. I actually got to meet David Tennant (who also plays
+                the 10th Doctor in Doctor Who and is my favorite Doctor) in person at ComicCon and got him to sign the original piece!</p></div>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-danger" @click="toggleDescription()">Description</button>
+        <button type="button" class="btn btn-danger" @click="toggleDescription()">Toggle Description</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="resetImg()">Close</button>
         </div>
         </div>
@@ -162,10 +173,13 @@
         </div>
         <div class="modal-body">
             <div v-show="showImage"><img class="card-img-top" src="../assets/Images/portraits/cutiepiesensei_cosplay.jpg" alt="Cutiepiesensei"></div>
-            <div v-show="showDescription"><p v-show="showDescription">Insert description here...</p></div>
+            <div v-show="showDescription"><p v-show="showDescription">CutiePieSensei is the username of an extremely talented cosplayer that I follow on Facebook.
+            For reference, cosplay is when someone dresses up as characters from movies, comics, manga, anime, TV, video games, etc. A lot of cosplayers painstakingly sew their
+            own costumes and build their own props. This particular cosplayer not only sewed, but designed her own costume based off of Marvel's Black Panther. This
+            was one of the first pieces were I decided that it needed a pop of color so I used a combination of colored pencils and acrylic paint to add the purple.</p></div>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-danger" @click="toggleDescription()">Description</button>
+        <button type="button" class="btn btn-danger" @click="toggleDescription()">Toggle Description</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="resetImg()">Close</button>
         </div>
         </div>
@@ -185,14 +199,19 @@
         <div class="modal-content">
         <div class="modal-header">
             <h4 class="modal-title">Marvel's Thor - Loki of Asgard</h4>
+            <p>"I am Loki of Asgard and I am burdened with glorious purpose." -Tom Hiddleston as Loki in The Avengers</p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
             <div v-show="showImage"><img class="card-img-top" src="../assets/Images/portraits/loki_of_asgard.jpg" alt="Loki"></div>
-            <div v-show="showDescription"><p v-show="showDescription">Insert description here...</p></div>
+            <div v-show="showDescription"><p v-show="showDescription">Loki is a character from the Marvel Universe who is known as the God of Mischief
+                and brother of Thor. His relationship to Thor starts off strained to say the least, but as his character has developed, he has gone from supervillain to 
+                more of an antihero. While he started as a recurring character in the Thor movies and the Avengers, he now has his own TV series. He's played by the
+                actor Tom Hiddleston who is one of my favorite actors and I absolutely love him as Loki.
+                </p></div>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-danger" @click="toggleDescription()">Description</button>
+        <button type="button" class="btn btn-danger" @click="toggleDescription()">Toggle Description</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="resetImg()">Close</button>
         </div>
         </div>
@@ -216,10 +235,12 @@
         </div>
         <div class="modal-body">
             <div v-show="showImage"><img class="card-img-top" src="../assets/Images/portraits/newly_weds.jpg" alt="Newly Weds Commission"></div>
-            <div v-show="showDescription"><p v-show="showDescription">Insert description here...</p></div>
+            <div v-show="showDescription"><p v-show="showDescription">This was a commission that I did for a lovely couple that was getting married. The husband
+                 commissioned me to draw one of their engagement photos as a surprise for his wife. Fortunately, I was able to get it completed in time for their wedding where they displayed
+                 it for all of their guests as you walked into the reception. This was such a great piece to work on and they loved it!</p></div>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-danger" @click="toggleDescription()">Description</button>
+        <button type="button" class="btn btn-danger" @click="toggleDescription()">Toggle Description</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="resetImg()">Close</button>
         </div>
         </div>
@@ -239,14 +260,16 @@
         <div class="modal-content">
         <div class="modal-header">
             <h4 class="modal-title">Final Fantasy VII - Tifa Lockhart</h4>
+            <p>"You've seen how much ass I can kick." -Tifa Lockhart</p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
             <div v-show="showImage"><img class="card-img-top" src="../assets/Images/portraits/tifa_lockhart.jpg" alt="FFVII - Tifa"></div>
-            <div v-show="showDescription"><p v-show="showDescription">Insert description here...</p></div>
+            <div v-show="showDescription"><p v-show="showDescription">Tifa Lockhart is a character from the video game 'Final Fantasy VII'. She's Cloud's
+                childhood friend, owner of the 7th Heaven bar, member of AVALANCHE, and a martial arts fighter.</p></div>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-danger" @click="toggleDescription()">Description</button>
+        <button type="button" class="btn btn-danger" @click="toggleDescription()">Toggle Description</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="resetImg()">Close</button>
         </div>
         </div>
@@ -266,6 +289,7 @@
         <div class="modal-content">
         <div class="modal-header">
             <h4 class="modal-title">Final Fantasy VII - Aerith</h4>
+            <p>quote here</p>
             <button type="button" class="btn-close" data-bs-dismiss="modal" @click="resetImg()"></button>
         </div>
         <div class="modal-body">
@@ -273,7 +297,7 @@
             <div v-show="showDescription"><p v-show="showDescription">Insert description here...</p></div>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-danger" @click="toggleDescription()">Description</button>
+        <button type="button" class="btn btn-danger" @click="toggleDescription()">Toggle Description</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="resetImg()">Close</button>
         </div>
         </div>
@@ -307,6 +331,10 @@ export default {
 </script>
 
 <style scoped>
+.modal-header{
+    display: flex;
+    flex-direction: column;
+}
 button{
     border: none;
 }
