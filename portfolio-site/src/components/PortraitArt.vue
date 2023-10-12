7e7bd37c78f3329d5bbbc52c4e469d7d2b535ece
ATaylorN <amandanagies@yahoo.com>
2023-10-11 20:10:47 -0400

    got couple of images to expand on click

--- portfolio-site/src/components/PortraitArt.vue
+++ portfolio-site/src/components/PortraitArt.vue
@@ -2,26 +2,82 @@
 <div class="portraits-page">
   <div class="portraits">
     
+    <button type="button" data-bs-toggle="modal" data-bs-target="#modal1">
         <div class="card">
-         <img class="card-img-top" src="../assets/Images/portraits/nux.jpg" alt="Nux">
-         <div class="card-img-overlay">
-        <h4 class="card-title">Nux</h4>
+            <img class="card-img-top" src="../assets/Images/portraits/nux.jpg" alt="Nux">
+            <div class="card-img-overlay">
+            <h4 class="card-title">Mad Max - Nux</h4>
+            </div>
+        </div>
+    </button>
+        <div class="modal" id="modal1">
+        <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Mad Max: Fury Road - Nux</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+            <img class="card-img-top" src="../assets/Images/portraits/nux.jpg" alt="Nux">
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        </div>
+        </div>
         </div>
         </div>
 
+        <button type="button" data-bs-toggle="modal" data-bs-target="#modal2">
         <div class="card">
-        <img class="card-img-top" src="../assets/Images/portraits/nux2.jpg" alt="Nux">
-        <div class="card-img-overlay">
-        <h4 class="card-title">Nux</h4>
+            <img class="card-img-top" src="../assets/Images/portraits/nux2.jpg" alt="Nux v2">
+            <div class="card-img-overlay">
+            <h4 class="card-title">Mad Max - Nux</h4>
+            </div>
+        </div>
+    </button>
+        <div class="modal" id="modal2">
+        <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Mad Max: Fury Road - Nux</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+            <img class="card-img-top" src="../assets/Images/portraits/nux2.jpg" alt="Nux">
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        </div>
+        </div>
         </div>
         </div>
 
+        <button type="button" data-bs-toggle="modal" data-bs-target="#modal3">
         <div class="card">
-        <img class="card-img-top" src="../assets/Images/portraits/chester_bennington.jpg" alt="Chester Bennington">
-        <div class="card-img-overlay">
-        <h4 class="card-title">Linkin Park - Chester Bennington</h4>
+            <img class="card-img-top" src="../assets/Images/portraits/chester_bennington.jpg" alt="Chester Bennington">
+            <div class="card-img-overlay">
+            <h4 class="card-title">Chester Bennington</h4>
+            </div>
+        </div>
+    </button>
+        <div class="modal" id="modal3">
+        <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Linkin Park - Chester Bennington</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+            <img class="card-img-top" src="../assets/Images/portraits/chester_bennington.jpg" alt="Chester Bennington">
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
         </div>
         </div>
+        </div>
+        </div>
+
+        
 
         <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/eleventh_doctor.jpg" alt="Dr.Who - 11th Doctor">
@@ -40,7 +96,7 @@
         <div class="card">
         <img class="card-img-top" src="../assets/Images/portraits/cutiepiesensei_cosplay.jpg" alt="Cutiepiesensei">
         <div class="card-img-overlay">
-        <h4 class="card-title">Black Panther Cosplay - Cutiepiesensei</h4>
+        <h4 class="card-title">Cutiepiesensei Cosplay</h4>
         </div>
         </div>
 
@@ -80,19 +136,25 @@
 
 <script>
 export default {
-
 }
 </script>
 
 <style scoped>
+button{
+    border: none;
+}
 .card img{
     width: 250px;
     height: 310px;
     object-fit: cover;
+    border-radius: 10px;
 }
 .card{
     box-shadow: 0px 5px 20px 0px black;
     border: none;
+}
+.card:hover{
+    border: 3px solid goldenrod;
 }
 .portraits{
     padding: 4rem;
@@ -104,6 +166,15 @@ export default {
 }
 .card-img-overlay{
     text-align: center;
+    margin-top: 250px;
+    background: linear-gradient(transparent, black);
+}
+.card h4 {
+    font-family: Oswald, sans-serif;
+    font-size: 20px;
+    font-weight: 600;
+    color: white;
+    
 }
 
 
